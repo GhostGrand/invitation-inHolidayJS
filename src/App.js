217bae8b0cf83eea806e167c7774ace92dca1ab7
Sergey <wer113wer@yandex.ru>
2022-12-26 19:11:22 +0300

    Сделано приглашение

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Invitation from "./Components/invitation";
+
+function App() {
+  return (
+    <div className="App">
+      <Invitation></Invitation>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/invitation.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Background from './background.png'
+import Logo from './logo.png'
+export default function invitation() {
+    
+    
+        
+  return (
+    
+      <div className='bg'>
+        {/* <img src={Background} alt="" className='bg'/> */}
+        <div className="container">
+            <div className='wrapper'>
+                <ul className='ul'>
+                    <li>
+                        <p className='first_text'>Дорогие</p>
+                        <p className='person_text'>Коллеги</p>
+                        <p className='invitation_text'>Приглашаем вас на корпоратив, посвященное празднованию нового года, которое состоится</p>
+                        <p className='date_text'>30 декабря</p>
+                    </li>
+                    <li className='list_element'>
+                        <img src={Logo} className="logo" />
+                    </li>
+                    <li>
+                        <input type="text" className='form' placeholder="Введите ваше ФИО" />
+                    </li>
+                    <li>
+                        <input type="number" className='form' placeholder="Введите ваш номер" />
+                    </li>
+                    <li>
+                        <button className='button'>Я буду!</button>
+                    </li>
+                </ul>
+            
+            </div>
+        </div>
+    </div>
+  )
+}
